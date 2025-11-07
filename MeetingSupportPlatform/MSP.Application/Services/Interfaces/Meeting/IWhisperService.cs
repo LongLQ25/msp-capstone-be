35d8babc46c095d931fc0ec87a4e246a3a133fe9
loc phuoc <phuocloc301203@gmail.com>
2025-11-07 23:44:26 +0700

    [DELETE] delete code not using

--- MeetingSupportPlatform/MSP.Application/Services/Interfaces/Meeting/IWhisperService.cs
+++
@@ -1,9 +0,0 @@
-﻿using MSP.Application.Models.Responses.Meeting;
-
-namespace MSP.Application.Services.Interfaces.Meeting
-{
-    public interface IWhisperService
-    {
-        Task<List<TranscriptionLine>> TranscribeVideoAsync(string videoPath);
-    }
-}
